c5c624221e117141c101ae897d288d98cecd1a6b
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-03 00:43:55 +0530

    done

---
+++ src/com/java/Day_02_Aug/FirstAndLastSumArr.java
@@ -0,0 +1,21 @@
+package com.java.Day_02_Aug;
+
+public class FirstAndLastSumArr {
+    public static void main(String[] args) {
+
+
+        int arr1[] = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int arr2[] = new int[]{2, 4, 1, 6, 7, 3, 9};
+        int arr[] = new int[arr1.length];
+
+        System.out.print("arr[i] : ");
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            arr[i] = arr1[i]+arr2[arr.length-i-1];
+
+            System.out.print (arr[i] + " ");
+
+        }
+    }
+}
---
+++ src/com/java/Day_02_Aug/PowerNumber.java
@@ -0,0 +1,20 @@
+package com.java.Day_02_Aug;
+
+import java.util.Scanner;
+
+public class PowerNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = Integer.parseInt(sc.next());
+
+        System.out.print("Enter the Power number : ");
+        int pow = Integer.parseInt(sc.next());
+        int res = 1 ;
+        for(int i = 1 ; i<=pow; i++){
+           res *=num ;
+        }
+        System.out.println("Power Of Number : " + res);
+
+    }
+}
