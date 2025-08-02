9eea34a3e9d79ecd2c454ec942c355080646e339
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-02 22:32:23 +0530

    main

--- src/com/java/Aug/Swap_Two_Number.java
+++ src/com/java/Aug/Swap_Two_Number.java
@@ -1,8 +1,22 @@
 package com.java.Aug;
 
+import java.util.Scanner;
+
 public class Swap_Two_Number {
     public static void main(String[] args) {
-        System.out.println("Lala lala");
-        System.out.println("Radha");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int firstNumber = Integer.parseInt(sc.next());
+
+        System.out.print("Enter the first number : ");
+        int secondNumber = Integer.parseInt(sc.next());
+
+        firstNumber = secondNumber+firstNumber;
+        secondNumber = firstNumber-secondNumber;
+        firstNumber = firstNumber-secondNumber;
+
+        System.out.println("Swaping fo Two Number :" + "\nfirstNumber :" +firstNumber +"\nSecondNumber :"+secondNumber);
+
     }
 }
