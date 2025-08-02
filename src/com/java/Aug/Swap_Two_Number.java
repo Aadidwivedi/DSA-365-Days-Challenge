9d119f1d59afb8ae1599a088b374ed1974d7eb61
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-02 21:57:40 +0530

    Radha

--- src/com/java/Aug/Swap_Two_Number.java
+++ src/com/java/Aug/Swap_Two_Number.java
@@ -3,5 +3,6 @@ package com.java.Aug;
 public class Swap_Two_Number {
     public static void main(String[] args) {
         System.out.println("Lala lala");
+        System.out.println("Radha");
     }
 }
