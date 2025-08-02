739ac189dee2d46695fd12369c270be14d0d3c8f
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-02 23:10:53 +0530

    change code

--- src/com/java/Aug/Add_Number_Two.java
+++ src/com/java/Aug/Add_Number_Two.java
@@ -1,7 +1,17 @@
 package com.java.Aug;
 
+import java.util.Scanner;
+
 public class Add_Number_Two {
     public static void main(String[] args) {
-        System.out.println("hhh");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int firstNumber = Integer.parseInt(sc.next());
+
+        System.out.print("Enter the first number : ");
+        int secondNumber = Integer.parseInt(sc.next());
+
+        System.out.println("Sum of Two Numbers :" +(firstNumber+secondNumber));
     }
 }
