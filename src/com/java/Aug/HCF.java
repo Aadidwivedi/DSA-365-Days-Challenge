8e96b2a90ee0d3f7df81d2854b0966df9d238034
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-02 23:36:20 +0530

    HCFOFTWONUMBWES

---
+++ src/com/java/Aug/HCF.java
@@ -0,0 +1,29 @@
+package com.java.Aug;
+
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int firstNumber = Integer.parseInt(sc.next());
+        System.out.print("Enter the first number : ");
+        int secondNumber = Integer.parseInt(sc.next());
+
+        int lowest = firstNumber>secondNumber?secondNumber:firstNumber;
+        int HCFOftwoNumbersIs = 1;
+
+        for(int i = 1 ;i<=lowest;i++){
+            if(firstNumber%i==0 &&secondNumber%i==0){
+
+                HCFOftwoNumbersIs = i ;
+            }
+
+        }
+
+        System.out.println("LCM OF Two Numbers : " +HCFOftwoNumbersIs);
+
+    }
+}
