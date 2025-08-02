555352ad20b3ed1d1bf68c023d6287dd87bed13a
Aadidwivedi <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-03 00:04:16 +0530

    LCM

---
+++ src/com/java/Aug/LCMOfTwoNumbers.java
@@ -0,0 +1,32 @@
+package com.java.Aug;
+
+import java.util.Scanner;
+
+public class LCMOfTwoNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int firstNumber = Integer.parseInt(sc.next());
+        System.out.print("Enter the first number : ");
+        int secondNumber = Integer.parseInt(sc.next());
+
+        int Highest = firstNumber<secondNumber?secondNumber:firstNumber;// uniery op
+
+        int LCMOftwoNumbersIs = 1;
+
+        for(int i = Highest ;i>=0;i++){
+            if(i%firstNumber==0 && i%secondNumber==0){
+
+                LCMOftwoNumbersIs = i ;
+                break;
+            }
+
+        }
+
+        System.out.println("LCM OF Two Numbers : " +LCMOftwoNumbersIs);
+
+    }
+
+    }
