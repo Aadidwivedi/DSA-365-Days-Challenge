3835e11f023b0790d6e72ec19589f7f788fef53f
Adarsh Dubey <122205468+Aadidwivedi@users.noreply.github.com>
2025-08-03 19:12:03 +0530

    Update DubleyCateArry.java

--- src/com/java/day_03_aug/DubleyCateArry.java
+++ src/com/java/day_03_aug/DubleyCateArry.java
@@ -6,7 +6,7 @@ public class DubleyCateArry {
 
         for(int j = 0;j<=9;j++) {
             int count = 0  ;
-            ;
+            
             for (int i = 0; i < arr.length; i++) {
                if(arr[i] == j){
                    count++;
